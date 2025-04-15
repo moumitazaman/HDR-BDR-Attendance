3503f732b1d6d4f9ecefc0a22754a327039a8feb
moumitasub <moumitasub@gmail.com>
2025-04-15 19:12:48 +0600

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- <h1>{{ $t("about.title") }}</h1>
+    <p>{{ $t("description") }}</p> -->
+  </div>
+  <div>
+    <DataTable />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DataTable from "@/components/DataTable.vue";
+</script>
+
+<style scoped></style>
